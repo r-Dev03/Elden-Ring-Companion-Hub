8d047c12b9f6669142b394da2f0696895fe4b970
Ribbal Hussain <ahmedhussain32000@gmail.com>
2023-04-14 23:54:34 -0500

    Added placeholder button for organizer. Added icon sprite for navbar.

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-      <div>Placeholder button</div>
+      <div className='organizerBtn'>Placeholder button</div>
     </div>
   );
 }
