a8befb53c6c9c8d67ff8791aef1735054f53683a
Ribbal Hussain <ahmedhussain32000@gmail.com>
2023-04-21 20:46:05 -0500

    General changes

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
             <Navbar />
             <div className='oBtnContainer'>
                 <button className='organizerBtn'>Organizer</button>
+                <Form />
             </div>
         </div>
     );
