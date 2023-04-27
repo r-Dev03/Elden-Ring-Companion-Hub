11782acedd4928c8c5fc22f2600e3685e6cd54a4
Ribbal Hussain <ahmedhussain32000@gmail.com>
2023-04-27 15:17:52 -0500

    Installed react router and react router dom for proper routing. Wrapped buttons from navbar in link tags and styled to make sure they don't interfere with existing design. Now four routes are existing,  Bosses, Areas, Crafting, and CheatSheet. Bosses route now routes to BossForm Component. Other routes still need appropriate forms.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,51 @@
 import './index.css';
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Navbar from './components/Navbar';
 import { useState, useEffect } from 'react'
 import BossForm from './components/BossForm.js'
 
 function App() {
-
     return (
-        <div className="App">
-            <Navbar />
-            <BossForm />
-            <button className='organizerBtn'>Organizer</button>
-        </div>
+        <Router>
+            <div className="App">
+                <Routes>
+                    <Route path='/' element={
+                        <>
+                            <Navbar />
+                            <button className='organizerBtn'>Organizer</button>
+                        </>} />
+                    <Route path='/Bosses' element={
+                        <>
+                            <Navbar />
+                            <button className='organizerBtn'>Organizer</button>
+                            <BossForm />
+                        </>
+                    } />
+                    <Route path='/Areas' element={
+                        <>
+                            <Navbar />
+                            <button className='organizerBtn'>Organizer</button>
+                            <h1> Information about areas will be here</h1>
+                        </>
+                    } />
+                    <Route path='/Crafting' element={
+                        <>
+                            <Navbar />
+                            <button className='organizerBtn'>Organizer</button>
+                            <h1>Crafting information will be here</h1>
+                        </>
+                    } />
+                    <Route path='/CheatSheet' element={
+                        <>
+                            <Navbar />
+                            <button className='organizerBtn'>Organizer</button>
+                            <h1>Infoormation fetched from elden ring api will be here</h1>
+                        </>
+                    } />
+                </Routes>
+            </div>
+        </Router>
     );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,24 +1,32 @@
-
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
     return (
         <div className="navbar_container">
-            <button className="button">
-                <span className="navsprite bosses"></span>
-                <span> Bosses </span>
-            </button>
-            <button className="button">
-                <span className="navsprite areas"></span>
-                <span> Areas </span>
-            </button>
-            <button className="button">
-                <span className="navsprite crafting"></span>
-                <span> Crafting </span>
-            </button>
-            <button className="button">
-                <span className="navsprite weapons"></span>
-                <span> Weapons </span>
-            </button>
+            <Link to={`/Bosses`}>
+                <button className="button">
+                    <span className="navsprite bosses"></span>
+                    <span> Bosses </span>
+                </button>
+            </Link>
+            <Link to={`/Areas`}>
+                <button className="button">
+                    <span className="navsprite areas"></span>
+                    <span> Areas </span>
+                </button>
+            </Link>
+            <Link to={`/Crafting`}>
+                <button className="button">
+                    <span className="navsprite crafting"></span>
+                    <span> Crafting </span>
+                </button>
+            </Link>
+            <Link to={`/CheatSheet`}>
+                <button className="button">
+                    <span className="navsprite paper"></span>
+                    <span> Cheat Sheet</span>
+                </button>
+            </Link>
         </div>
     )
 }
