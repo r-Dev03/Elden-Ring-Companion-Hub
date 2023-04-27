77e4dd368c6c936f045ad6880cbba30aa7fb556b
Ribbal Hussain <ahmedhussain32000@gmail.com>
2023-04-26 21:31:00 -0500

    Removed setBossForm and setShowBossForm states.

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import { useState, useEffect } from 'react'
 import BossForm from './components/BossForm.js'
 
 function App() {
-    const [showBossForm, setShowBossForm] = useState(false)
 
     return (
         <div className="App">
