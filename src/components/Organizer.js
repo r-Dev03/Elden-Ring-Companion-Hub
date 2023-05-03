df75067878b7afb732b0184025c8b7d056d5f1ad
Ribbal Hussain <ahmedhussain32000@gmail.com>
2023-05-03 16:57:30 -0500

    Mapped through other 2 arrays within organizer function to allow proper rendering for each type of log. Currently looking into more effecient methods. Integrated conditional rendering within oranizer to prevent crashing if no local storage key exists for any type of form.

--- src/components/Organizer.js
+++ src/components/Organizer.js
@@ -1,29 +1,55 @@
 import React from "react";
 
 const Organizer = () => {
-    const bosses = JSON.parse(window.localStorage.getItem('bossLogs'))
-    //Testing options in regards to rendering multiple elements and mapping through 3 different arrays.
-    Object.keys(localStorage).map(logType => {
-        JSON.parse(window.localStorage.getItem(logType)).map(el => {
-            console.log(el)
-        })
-    })
+    const areaLogs = JSON.parse(window.localStorage.getItem('areaLogs'))
+    const bossLogs = JSON.parse(window.localStorage.getItem('bossLogs'))
+    const craftingLogs = JSON.parse(window.localStorage.getItem('craftingLogs'))
 
     //here is the return for the Organizer
     return (
         <>
-            {bosses.map(el => {
-                //here's the return for the loop
-                return (
-                    <div key={el.id}>
-                        <p>{el.bossName}</p>
-                        <p>{el.bossType}</p>
-                        <p>{el.bossAttackPatterns}</p>
-                        <p>{el.bossCounterAttacks}</p>
-                        <p>{el.bossInfo}</p>
-                    </div>
-                )
-            })}
+            <div className="logsContainer">
+                {bossLogs ? bossLogs.map(el => {
+                    //here's the return for the loop
+                    return (
+                        <div className="bossLogsContainer">
+                            <div key={el.id}>
+                                <p>{el.bossName}</p>
+                                <p>{el.bossType}</p>
+                                <p>{el.bossAttackPatterns}</p>
+                                <p>{el.bossCounterAttacks}</p>
+                                <p>{el.bossInfo}</p>
+                            </div>
+                        </div>
+                    )
+                }) : <p>No Boss Strategies? Git Gud</p>}
+
+                {areaLogs ? areaLogs.map(el => {
+                    return (
+                        <div className="areaLogsContainer">
+                            <div key={el.id}>
+                                <p>{el.areaName}</p>
+                                <p>{el.areaQuirks}</p>
+                                <p>{el.commonEnemies}</p>
+                                <p>{el.areaInfo}</p>
+                            </div>
+                        </div>
+                    )
+                }) : <p>No Area Notes? Git Gud</p>}
+
+                {craftingLogs ? craftingLogs.map(el => {
+                    return (
+                        <div className="craftingLogsContainer">
+                            <div key={el.id}>
+                                <p>{el.itemName}</p>
+                                <p>{el.craftingRecipe}</p>
+                                <p>{el.materialLocations}</p>
+                                <p>{el.itemInfo}</p>
+                            </div>
+                        </div>
+                    )
+                }) : <p>No Crafting Recipes? Git Gud</p>}
+            </div>
         </>
     )
 }
