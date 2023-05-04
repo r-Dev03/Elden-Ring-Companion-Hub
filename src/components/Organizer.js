bf630190b4f0a99274e17458d9c66960b0f3c22c
Ribbal Hussain <ahmedhussain32000@gmail.com>
2023-05-04 17:23:50 -0500

    Removed styling for books as span background images. Resized images and am now pulling them from cloudinary.

--- src/components/Organizer.js
+++ src/components/Organizer.js
@@ -10,51 +10,49 @@ const Organizer = () => {
     return (
         <>
             <div className="logsContainer">
-                    {bossLogs ? bossLogs.map(el => {
-                        //here's the return for the loop
-                        return (
-                            <div className="bossLogsContainer">
-                            <span className="bossBook"></span>
-                                <div className="boss" key={el.id}>
-                                    <p>{el.bossName}</p>
-                                    <p>{el.bossType}</p>
-                                    <p>{el.bossAttackPatterns}</p>
-                                    <p>{el.bossCounterAttacks}</p>
-                                    <p>{el.bossInfo}</p>
-                                </div>
+                {bossLogs ? bossLogs.map(el => {
+                    //here's the return for the loop
+                    return (
+                        <div className="bossLogsContainer">
+                        <img src="https://res.cloudinary.com/dtyc44fjq/image/upload/v1683236340/Crafting__Book2_s6sxjr.png"></img>
+                            <div className="boss" key={el.id}>
+                                <p>{el.bossName}</p>
+                                <p>{el.bossType}</p>
+                                <p>{el.bossAttackPatterns}</p>
+                                <p>{el.bossCounterAttacks}</p>
+                                <p>{el.bossInfo}</p>
                             </div>
-                        )
-                    }) : <p>No Boss Strategies? Git Gud</p>}
+                        </div>
+                    )
+                }) : <p>No Boss Strategies? Git Gud</p>}
 
-                    {areaLogs ? areaLogs.map(el => {
-                        return (
-                            <div className="areaBook">
-                                <div className="areaLogsContainer">
-                                    <div key={el.id}>
-                                        <p>{el.areaName}</p>
-                                        <p>{el.areaQuirks}</p>
-                                        <p>{el.commonEnemies}</p>
-                                        <p>{el.areaInfo}</p>
-                                    </div>
-                                </div>
+                {areaLogs ? areaLogs.map(el => {
+                    return (
+                        <div className="areaLogsContainer">
+                        <img src="https://res.cloudinary.com/dtyc44fjq/image/upload/v1683236340/Crafting_Book1_ahtlbr.png"></img>
+                            <div className="area" key={el.id}>
+                                <p>{el.areaName}</p>
+                                <p>{el.areaQuirks}</p>
+                                <p>{el.commonEnemies}</p>
+                                <p>{el.areaInfo}</p>
                             </div>
-                        )
-                    }) : <p>No Area Notes? Git Gud</p>}
+                        </div>
+                    )
+                }) : <p>No Area Notes? Git Gud</p>}
 
-                    {craftingLogs ? craftingLogs.map(el => {
-                        return (
-                            <div className="craftingBook">
-                                <div className="craftingLogsContainer">
-                                    <div key={el.id}>
-                                        <p>{el.itemName}</p>
-                                        <p>{el.craftingRecipe}</p>
-                                        <p>{el.materialLocations}</p>
-                                        <p>{el.itemInfo}</p>
-                                    </div>
-                                </div>
+                {craftingLogs ? craftingLogs.map(el => {
+                    return (
+                        <div className="craftingLogsContainer">
+                        <img src="https://res.cloudinary.com/dtyc44fjq/image/upload/v1683236340/Crafting_Book3_gtafbj.png"></img>
+                            <div className="item" key={el.id}>
+                                <p>{el.itemName}</p>
+                                <p>{el.craftingRecipe}</p>
+                                <p>{el.materialLocations}</p>
+                                <p>{el.itemInfo}</p>
                             </div>
-                        )
-                    }) : <p>No Crafting Recipes? Git Gud</p>}
+                        </div>
+                    )
+                }) : <p>No Crafting Recipes? Git Gud</p>}
             </div>
         </>
     )
