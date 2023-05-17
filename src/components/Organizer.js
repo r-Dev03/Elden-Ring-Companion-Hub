b8a48592b998fa287ac460d85abd11fd7a16f111
Ribbal Hussain <ahmedhussain32000@gmail.com>
2023-05-16 22:40:15 -0500

    Re-added toggle log functionality and added new delete icon. Functionality is still being implemented.

--- src/components/Organizer.js
+++ src/components/Organizer.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { FaRegTimesCircle } from "react-icons/fa";
+import { FaRegTimesCircle, FaTrash } from "react-icons/fa";
 import BossForm from "./BossForm";
 import AreaForm from "./AreaForm";
 import CraftingForm from "./CraftingForm";
@@ -22,22 +22,42 @@ const Organizer = () => {
                     {bossLogs ? bossLogs.map(el => {
                         //here's the return for the loop
                         return (
-                            <label className="tableLabel">{el.bossName}</label>
-
+                            <>
+                                <label className="tableLabel" onClick={() => toggleLog(el.id)}>{el.bossName}</label>
+                                <div className={selectedLog == el.id ? "openLog" : "hideLog"} key={el.id}>
+                                    <button className="close-btn" onClick={() => toggleLog(el.id)}> <FaRegTimesCircle /></button>
+                                    <button className="delete-btn"><FaTrash /></button>
+                                    <BossForm preLoadedData={el} />
+                                </div>
+                            </>
                         )
                     }) : <p>No Boss Strategies? Git Gud</p>}
                 </tbody>
                 <tbody>
                     {areaLogs ? areaLogs.map(el => {
                         return (
-                            <label className="tableLabel">{el.areaName}</label>
+                            <>
+                                <label className="tableLabel" onClick={() => toggleLog(el.id)}>{el.areaName}</label>
+                                <div className={selectedLog == el.id ? "openLog" : "hideLog"} key={el.id}>
+                                    <button className="close-btn" onClick={() => toggleLog(el.id)}> <FaRegTimesCircle /></button>
+                                    <button className="delete-btn"><FaTrash /></button>
+                                    <AreaForm preLoadedData={el} />
+                                </div>
+                            </>
                         )
                     }) : <p>No Area Notes? Git Gud</p>}
                 </tbody>
                 <tbody>
                     {craftingLogs ? craftingLogs.map(el => {
                         return (
-                            <label className="tableLabel">{el.itemName}</label>
+                            <>
+                                <label className="tableLabel" onClick={() => toggleLog(el.id)}>{el.itemName}</label>
+                                <div className={selectedLog == el.id ? "openLog" : "hideLog"} key={el.id}>
+                                    <button className="close-btn" onClick={() => toggleLog(el.id)}> <FaRegTimesCircle /></button>
+                                    <button className="delete-btn"><FaTrash /></button>
+                                    <CraftingForm preLoadedData={el} />
+                                </div>
+                            </>
                         )
                     }) : <p>No Crafting Recipes? Git Gud</p>}
                 </tbody>
