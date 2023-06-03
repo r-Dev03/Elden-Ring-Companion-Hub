14fa31d5511f4ee4448daa2213becac930456fbc
Ribbal Hussain <ahmedhussain32000@gmail.com>
2023-06-03 11:41:05 -0500

    Got axios get request to work and render image based on input, currently working on parsing input to allow for consistent get requests.

--- src/components/CheatSheet.js
+++ src/components/CheatSheet.js
@@ -5,17 +5,20 @@ const CheatSheet = () => {
     const [inputValue, setInputValue] = useState('')
     const [categoryValue, setCategoryValue] = useState('Ammos')
     const [responseData, setResponseData] = useState(null)
-    
-    const fetchData = () => {
-        try {
-            const response = axios.get(`https://eldenring.fanapis.com/api/${categoryValue}?name=${inputValue}`)
-            setResponseData(response.data);
-            console.log(responseData)
-        } catch (error) {
-            console.error('Error fetching data:', error);
-        }
-    };
 
+    const exampleURL = "https://eldenring.fanapis.com/api/ashes?name=Ash%20Of%20War:%20Prelate's%20Charge"
+    
+       const fetchData =  () => {
+        axios.get(`https://eldenring.fanapis.com/api/${categoryValue}?name=${inputValue}`)
+            .then(res => {
+                setResponseData(res.data.data)
+                console.log(responseData)
+            }) 
+            .catch(err => {
+                console.log(err)
+            })
+
+    }
 
 
 
@@ -66,7 +69,7 @@ const CheatSheet = () => {
                 <input type='submit' value='Search' className='btn btn-block' />
             </div>
         </form>
-            <img className="data-img" src=""/>
+            <img className="data-img" src={responseData[0].image}/>
             <section className="data-info"></section>
 
         </div>
